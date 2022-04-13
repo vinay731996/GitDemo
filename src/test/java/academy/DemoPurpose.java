c5e0e4a522f337267f11051c1f6de7751160f788
vinayvinay1 <vinay731996>
2022-04-13 16:57:22 +0530

    x

--- src/test/java/academy/DemoPurpose.java
+++ src/test/java/academy/DemoPurpose.java
@@ -4,6 +4,14 @@ public class DemoPurpose {
 	
 public static void maint(String[] args) {
 	
+
 	System.out.println("vinay is good boy");
+
+	System.out.println("vinay is good boy");
+
+	System.out.println("vinay is good boy");
+	
+	System.out.println("vinay is good boy");
+
 }
 }
